26960564113979af99262fd19311d6b1ffe26b78
meh <meh@schizofreni.co>
2016-04-02 16:27:27 +0200

    controller/bin/play: allow using pads for play

--- controller/src/bin/play.rs
+++ controller/src/bin/play.rs
@@ -5,6 +5,11 @@ use controller::sound::Note;
 use std::time::Duration;
 use std::ops::Deref;
 
+trait Update {
+	fn update(&mut self, buttons: controller::Button, pad: controller::Pad);
+	fn has_update(&self) -> bool;
+}
+
 #[derive(Clone, Copy, PartialEq, Debug)]
 enum Button {
 	A,
@@ -40,24 +45,46 @@ struct Right {
 	current:  State,
 }
 
-impl Right {
-	pub fn update(&mut self, buttons: controller::Button) {
+impl Update for Right {
+	fn update(&mut self, buttons: controller::Button, pad: controller::Pad) {
 		self.previous = self.current;
 
-		if buttons.contains(button::A) {
-			self.current.button = Some(Button::A);
-		}
-		else if buttons.contains(button::X) {
-			self.current.button = Some(Button::B);
-		}
-		else if buttons.contains(button::Y) {
-			self.current.button = Some(Button::C);
-		}
-		else if buttons.contains(button::B) {
-			self.current.button = Some(Button::D);
+		if pad.right.x != 0 || pad.right.y != 0 {
+			let x = pad.left.x;
+			let y = pad.left.y;
+
+			if y < -15_000 && x > -15_000 && x < 15_000 {
+				self.current.button = Some(Button::A);
+			}
+			else if x < -15_000 && y > -15_000 && y < 15_000 {
+				self.current.button = Some(Button::B);
+			}
+			else if y > 15_000 && x > -15_000 && x < 15_000 {
+				self.current.button = Some(Button::C);
+			}
+			else if x > 15_000 && y > -15_000 && y < 15_000 {
+				self.current.button = Some(Button::D);
+			}
+			else {
+				self.current.button = None;
+			}
 		}
 		else {
-			self.current.button = None;
+			if buttons.contains(button::A) {
+				self.current.button = Some(Button::A);
+			}
+			else if buttons.contains(button::X) {
+				self.current.button = Some(Button::B);
+			}
+			else if buttons.contains(button::Y) {
+				self.current.button = Some(Button::C);
+			}
+			else if buttons.contains(button::B) {
+				self.current.button = Some(Button::D);
+			}
+			else {
+				self.current.button = None;
+			}
 		}
 
 		self.current.grip    = buttons.contains(button::RIGHT_GRIP);
@@ -74,7 +101,7 @@ impl Right {
 		}
 	}
 
-	pub fn has_update(&self) -> bool {
+	fn has_update(&self) -> bool {
 		self.previous != self.current
 	}
 }
@@ -93,11 +120,11 @@ struct Left {
 	current:  State,
 }
 
-impl Left {
-	pub fn update(&mut self, buttons: controller::Button, pad: controller::Pad) {
+impl Update for Left {
+	fn update(&mut self, buttons: controller::Button, pad: controller::Pad) {
 		self.previous = self.current;
 
-		if !buttons.contains(button::PAD_TOUCH) && (pad.left.x != 0 || pad.left.y != 0) {
+		if pad.left.x != 0 || pad.left.y != 0 {
 			let x = pad.left.x;
 			let y = pad.left.y;
 
@@ -135,7 +162,7 @@ impl Left {
 		}
 	}
 
-	pub fn has_update(&self) -> bool {
+	fn has_update(&self) -> bool {
 		self.previous != self.current
 	}
 }
@@ -151,23 +178,7 @@ impl Deref for Left {
 fn build<'a, 'b>(mut builder: controller::Sound<'a, 'b>, state: &State) -> controller::Sound<'a, 'b> {
 	let button = state.button.unwrap();
 
-	if state.grip && state.trigger {
-		builder = match button {
-			Button::A => builder.note(Note::D).octave(state.octave + 1),
-			Button::B => builder.note(Note::E).octave(state.octave + 1),
-			Button::C => builder.note(Note::F).octave(state.octave + 1),
-			Button::D => builder.note(Note::G).octave(state.octave + 1),
-		}
-	}
-	else if state.grip {
-		builder = match button {
-			Button::A => builder.note(Note::F).octave(state.octave - 1),
-			Button::B => builder.note(Note::G).octave(state.octave - 1),
-			Button::C => builder.note(Note::A).octave(state.octave - 1),
-			Button::D => builder.note(Note::B).octave(state.octave - 1),
-		}
-	}
-	else if state.trigger {
+	if state.trigger {
 		builder = match button {
 			Button::A => builder.note(Note::G).octave(state.octave),
 			Button::B => builder.note(Note::A).octave(state.octave),
@@ -184,7 +195,7 @@ fn build<'a, 'b>(mut builder: controller::Sound<'a, 'b>, state: &State) -> contr
 		}
 	};
 
-	if state.bumper {
+	if state.grip {
 		builder = builder.sharp();
 	}
 
@@ -202,7 +213,7 @@ fn main() {
 		match controller.state(Duration::from_secs(0)).unwrap() {
 			controller::State::Input { buttons, pad, .. } => {
 				left.update(buttons, pad);
-				right.update(buttons);
+				right.update(buttons, pad);
 
 				if left.has_update() {
 					if left.button.is_some() {
