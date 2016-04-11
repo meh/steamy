a521c601c1ff46af6d99c98bf13d3069971f9034
meh <meh@schizofreni.co>
2016-04-11 17:02:55 +0200

    controller/bin/victory: simplify macro

--- controller/src/bin/victory.rs
+++ controller/src/bin/victory.rs
@@ -1,36 +1,36 @@
 extern crate steamy_controller as controller;
-use controller::sound::Note;
 
-use std::time::Duration;
+use controller::sound::Note;
 use std::thread;
+use std::time::Duration;
 
 macro_rules! play {
 	($controller:expr, ?, $ms:expr) => {
-		::std::thread::sleep(Duration::from_millis($ms));
+		thread::sleep(Duration::from_millis($ms));
 	};
 
 	($controller:expr, $note:ident, $ms:expr) => {
 		$controller.sound().left().note(Note::$note).duration(Duration::from_millis($ms)).play().unwrap();
 		$controller.sound().right().note(Note::$note).duration(Duration::from_millis($ms)).play().unwrap();
-		::std::thread::sleep(Duration::from_millis($ms));
+		thread::sleep(Duration::from_millis($ms));
 	};
 
 	($controller:expr, $note:ident #, $ms:expr) => {
 		$controller.sound().left().note(Note::$note).sharp().duration(Duration::from_millis($ms)).play().unwrap();
 		$controller.sound().right().note(Note::$note).sharp().duration(Duration::from_millis($ms)).play().unwrap();
-		::std::thread::sleep(Duration::from_millis($ms));
+		thread::sleep(Duration::from_millis($ms));
 	};
 
 	($controller:expr, $note:ident, $oct:expr, $ms:expr) => {
 		$controller.sound().left().note(Note::$note).octave($oct).duration(Duration::from_millis($ms)).play().unwrap();
 		$controller.sound().right().note(Note::$note).octave($oct).duration(Duration::from_millis($ms)).play().unwrap();
-		::std::thread::sleep(Duration::from_millis($ms));
+		thread::sleep(Duration::from_millis($ms));
 	};
 
 	($controller:expr, $note:ident #, $oct:expr, $ms:expr) => {
 		$controller.sound().left().note(Note::$note).sharp().octave($oct).duration(Duration::from_millis($ms)).play().unwrap();
 		$controller.sound().right().note(Note::$note).sharp().octave($oct).duration(Duration::from_millis($ms)).play().unwrap();
-		::std::thread::sleep(Duration::from_millis($ms));
+		thread::sleep(Duration::from_millis($ms));
 	};
 }
 
